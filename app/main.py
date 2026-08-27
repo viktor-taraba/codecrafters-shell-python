78ae9f1a80669b2cd0cdb45f9d884a77bb17138d
viktor-taraba <victortaraba@gmail.com>
2026-08-27 21:18:12 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 import sys
 import os
 import itertools 
+import subprocess
 
 builtin_commands = ["exit", "echo", "type"]
+
+
+def is_executable(file_name):
+    "helper function to check whether file_name is executable program"
+
+    PATH = os.environ.get('PATH').split(os.pathsep)
+    extensions = [''] # Always check the exact name first
+    path_extensions = os.environ.get('PATHEXT', '.COM;.EXE;.BAT;.CMD').split(os.pathsep)
+    extensions.extend([ext.lower() for ext in path_extensions])
+
+    for directory, ext in itertools.product(PATH, extensions):
+        file_path = os.path.join(directory, file_name + ext)
+        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+            return file_path
+        else:
+            pass
+    else:
+        return None
+
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
@@ -12,7 +32,6 @@ def main():
 
         if command.lower() == "exit":
             break
-
         elif command.startswith("echo "):
             print(command[5:])
 
@@ -20,28 +39,22 @@ def main():
             command_param = command[5:].strip()
             if command_param in builtin_commands:
                 print(f"{command_param} is a shell builtin")
-            
-            # locate executable files
             else:
-            # go through every directory in PATH 
-                PATH = os.environ.get('PATH').split(os.pathsep)
-                extensions = [''] # Always check the exact name first
-                path_extensions = os.environ.get('PATHEXT', '.COM;.EXE;.BAT;.CMD').split(os.pathsep)
-                extensions.extend([ext.lower() for ext in path_extensions])
-
-                for directory, ext in itertools.product(PATH, extensions):
-                    file_path = os.path.join(directory, command_param + ext)
-
-                    if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
-                        print(f"{command_param} is {file_path}")
-                        break
-                    else:
-                        pass
+                # locate executable files - go through every directory in PATH 
+                file_path = is_executable(command_param)
+                if file_path:
+                    print(f"{command_param} is {file_path}")
                 else: # no file located in the fir loop
                     print(f"{command_param}: not found")
 
-        else:
-            sys.stdout.write(f"{command}: command not found\n")
+        else: # run a program
+            program_parts = command.split()
+            program_name = program_parts[0]
+
+            if is_executable(program_name):
+                subprocess.run(program_parts)
+            else:
+                sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
